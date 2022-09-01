dd72fc07b353ad93bc004fdad8332c65be5be6a1
hamidreza <takloohamidreza@gmail.com>
2022-09-01 11:44:34 +0430

    Append row as child element to container

--- scipt.js
+++ scipt.js
@@ -3,6 +3,7 @@ const container = document.querySelector('.container');
 for(let i =0; i<16; i++){
     const row = document.createElement('div');
     row.classList.add('row');
+    container.appendChild(row);
     for(let j =0; j<16; ++j){
         
     }
