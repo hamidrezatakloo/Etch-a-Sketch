5b3d687825997d1c68774c24020d0f2792c7f661
hamidreza <takloohamidreza@gmail.com>
2022-09-01 11:53:02 +0430

    Add square as childElement to row div

--- scipt.js
+++ scipt.js
@@ -7,5 +7,6 @@ for(let i =0; i<16; i++){
     for(let j =0; j<16; ++j){
         const square = document.createElement('div');
         square.classList.add('square');
+        row.appendChild(square);
     }
 }
