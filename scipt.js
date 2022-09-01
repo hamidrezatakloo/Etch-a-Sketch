169c6b4e9acf77b213bad0e9c475b494a59d0734
hamidreza <takloohamidreza@gmail.com>
2022-09-01 11:51:44 +0430

    Add class with name of 'square' to square element

--- scipt.js
+++ scipt.js
@@ -6,5 +6,6 @@ for(let i =0; i<16; i++){
     container.appendChild(row);
     for(let j =0; j<16; ++j){
         const square = document.createElement('div');
+        square.classList.add('square');
     }
 }
